a920ff8d0799ef4b9606c68a9f382fe704076944
gaboesmeden <68702323+gaboesmeden@users.noreply.github.com>
2026-07-23 21:37:21 -0500

    Update AUDITORIA_TRANSACCIONES insert statement
    modifique el segundo trigger, agregue id_prestamos ya que se tendria que poder rastrear debido a que prestamos estamos haciendo la sancion

--- plsql.sql_base.sql
+++ plsql.sql_base.sql
@@ -28,9 +28,9 @@ AFTER INSERT ON SANCION
 FOR EACH ROW
 BEGIN
     INSERT INTO AUDITORIA_TRANSACCIONES 
-    (id_administrador, tipo_evento, detalle_evento, usuario_auditor)
+    (id_prestamo, id_administrador, tipo_evento, detalle_evento, usuario_auditor)
     VALUES 
-    (NEW.id_administrador, 'NUEVA_SANCION', CONCAT('Sanción aplicada al usuario ', NEW.id_usuario, ' Motivo: ', NEW.motivo), CURRENT_USER());
+    (NEW.id_prestamo, NEW.id_administrador, 'NUEVA_SANCION', CONCAT('Sanción ID: ', NEW.id_sancion, ' aplicada al usuario ', NEW.id_usuario, '. Motivo: ', NEW.motivo, '. Multa: $', IFNULL(NEW.monto_multa, 0)), CURRENT_USER());
 END //
 
 -- ==========================================
